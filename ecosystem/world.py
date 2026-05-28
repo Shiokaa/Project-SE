8ab9a78da4825bd139b804280c5b10da340c5957
Shioka <tom.amaru09@gmail.com>
2026-05-28 23:06:17 +0200

    feat: update world avec de la poo, création du monde à l'initialisation

--- ecosystem/world.py
+++ ecosystem/world.py
@@ -1,29 +1,31 @@
 import noise
 import random
+from ecosystem.field import Field
 
-# Fonction pour créer le monde.
-# Le monde est divisé en 5 biomes :
-#       - Eaux profondes
-#       - Eaux
-#       - Terres
-#       - Herbes
-#       - Forêts
-def world(y, x, scale, octaves):
-    world = [[] for _ in range(y)]
-    seed = random.randint(0, 10000)
-    for height in range(y):
-        for width in range(x):
-            nx = width / scale + seed
-            ny = height / scale + seed
-            value = noise.pnoise2(ny, nx, octaves=octaves)
-            if value < -0.36:
-                world[height].append("ep")
-            elif value < -0.16:
-                world[height].append("e")
-            elif value < -0.06:
-                world[height].append("t")
-            elif value < 0.14:
-                world[height].append("h")
-            else:
-                world[height].append("f")
-    return world
+class World:
+    # Fonction pour créer le monde.
+    # Le monde est divisé en 5 biomes :
+    #       - Eaux profondes
+    #       - Eaux
+    #       - Terres
+    #       - Herbes
+    #       - Forêts
+    def __init__(self, y: int, x: int, scale: float, octaves: int) -> None:
+        world = [[] for _ in range(y)]
+        seed = random.randint(0, 10000)
+        for height in range(y):
+            for width in range(x):
+                nx = width / scale + seed
+                ny = height / scale + seed
+                value = noise.pnoise2(ny, nx, octaves=octaves)
+                if value < -0.36:
+                    world[height].append(Field.EAU_PROFONDE)
+                elif value < -0.16:
+                    world[height].append(Field.EAU)
+                elif value < -0.06:
+                    world[height].append(Field.TERRE)
+                elif value < 0.14:
+                    world[height].append(Field.HERBE)
+                else:
+                    world[height].append(Field.FORET)
+        self.grid = world
