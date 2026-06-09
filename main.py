52e2a3a2e838f7b3850d9a0a9ff05c6754bd74a0
Shiokaa <tom.amaru09@gmail.com>
2026-06-09 17:09:37 +0200

    feat: ajout du render pygame pour l'affichage

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ecosystem.world import World
 from renderers.image import render
 from renderers.terminal import display
+from renderers.game import run
 
 w = World(50, 100, 10, 1, 200)
 
+run(w.grid)
 render(w.grid)
 display(w.grid)
